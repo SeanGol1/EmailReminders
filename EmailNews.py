68a667eca9f52cb6e07d92224a665927170dfd54
seangol1 <Seangol1@hotmail.com>
2021-09-30 22:57:19 +0100

    Set up Config file

--- EmailNews.py
+++ EmailNews.py
@@ -1,41 +1,25 @@
-from idna import unicode
+#from idna import unicode
 from newsapi import NewsApiClient
 import json
 from datetime import date, timedelta
 import smtplib, ssl
-#from django.utils.encoding import smart_str, smart_unicode
+from configparser import ConfigParser
 
-
-newsapi = NewsApiClient(api_key='94454bdacf3247a2957f23c8de7f597f')
-
+# Set up 
+configfile = 'config.ini'
+config = ConfigParser()
+config.read(configfile)
+newsapi = NewsApiClient(api_key=config['api']['api_key'])
 todaydate = date.today()
 yestdate = todaydate - timedelta(days=1)
+emailstr = ""
+
 
 # Top Tech headlines
 tech_headlines = newsapi.get_top_headlines(sources='mashable,techcrunch.com')
 
 # Top Irish headlines    
-
 irish_headlines = newsapi.get_top_headlines(country='ie')
-
-#  Top World headlines
-
-
-
-#all_articles = newsapi.get_top_headlines(q='bitcoin',
- #                                     sources='bbc-news,the-verge',
-  #                                     domains= 'bbc.co.uk,techcrunch.com',
-   #                                    from_param='2017-12-01',
-    #                                   to='2017-12-12',
-     #                                  language='en',
-      #                                 sort_by='relevancy',
-       #                                page=2)
-  #sources = newsapi.get_sources()
-
-#news = json.loads(top_headlines)
-emailstr = ""
-
-
 
 
 
@@ -46,10 +30,6 @@ emailstr += " Stories\r\n\r\n"
 for items in irish_headlines:
     if(items == 'articles'):
         for articles in irish_headlines[items]:
-            #print(articles)
-            #if (articles == 'source'):
-                #for source in articles:
-                    #emailstr += source['name']
             emailstr += articles['title']
             emailstr += "\r\n\r\n"
             emailstr += articles['content']
@@ -58,21 +38,14 @@ for items in irish_headlines:
             emailstr += "\r\n___________________________________________________________________________________\r\n"
 
 
-
-
-
-
 ########  Tech Headlines
 emailstr += "======================================================================================\r\n\r\n"
 emailstr += "Tech Headlines - "
 emailstr += str(tech_headlines['totalResults'])
 emailstr += " Stories\r\n\r\n"
 for items in tech_headlines:
-     #print(items)
      if(items == 'articles'):
          for articles in tech_headlines[items]:
-             #print(articles)
-
              emailstr += articles['title']
              emailstr += "\r\n"
              emailstr += articles['content']
@@ -81,27 +54,26 @@ for items in tech_headlines:
              emailstr += "\r\n_______________________________\r\n"
 
 
+# Take from config file
+sender_email = config['email']['sender_email']
+sender_password = config['email']['sender_password']
+sender_email_port = config['email']['sender_email_port']
+receiver_email = config['email']['receiver_email']
 
-#print(emailstr)
-#print(json.dumps(irish_headlines, indent = 4))
-
-sender_email = ""
-receiver_email = ""
+# Set up a default heading for the email
 message = """\
 Subject: News Headlines 
 
 """
 
 message += emailstr
-#message.encode("ascii", errors="ignore")
-#rest_array = [text.encode("utf8") for text in message]
 
 port = 587  # For starttls
 context = ssl.create_default_context()
-with smtplib.SMTP("smtp.gmail.com", port) as server:
-     server.ehlo()  # Can be omitted
+with smtplib.SMTP(sender_email_port, port) as server:
+     server.ehlo()  
      server.starttls(context=context)
-     server.ehlo()  # Can be omitted
-     server.login(sender_email, "") #Input PW
+     server.ehlo()  
+     server.login(sender_email, sender_password)
      server.sendmail(sender_email, receiver_email, message.encode("utf-8"))
 
---
+++ temp/getrequestexample.py
@@ -0,0 +1,12 @@
+# Example of Article Get
+#all_articles = newsapi.get_top_headlines(q='bitcoin',
+ #                                     sources='bbc-news,the-verge',
+  #                                     domains= 'bbc.co.uk,techcrunch.com',
+   #                                    from_param='2017-12-01',
+    #                                   to='2017-12-12',
+     #                                  language='en',
+      #                                 sort_by='relevancy',
+       #                                page=2)
+  #sources = newsapi.get_sources()
+
+#news = json.loads(top_headlines)
